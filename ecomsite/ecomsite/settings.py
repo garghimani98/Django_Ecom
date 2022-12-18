5ba6e10b22023a3738860243504d82ab4d53f4e6
garghimani98 <garghimani98@gmail.com>
2022-12-18 15:48:21 +0530

    gitignore file is working

--- ecomsite/ecomsite/settings.py
+++ ecomsite/ecomsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import  config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*h(j*yqym&t9u2d$%dme&70+5%z)vgj6epm!bq$9p&$8w*+cf5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
